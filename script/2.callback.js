26573f5989537f01216ecb902b4360e9590e1887
Ashahab007 <ashahab007@gmail.com>
2025-03-07 16:29:36 +0600

    callback function

--- script/2.callback.js
+++ script/2.callback.js
@@ -1,2 +1,13 @@
 // ! Callback Function
 // function that is passed as a parameter of an another function is called a callback function
+
+function ghotok(callback, patriName) {
+  callback(patriName);
+}
+
+function patri(patriName) {
+  console.log("first marriage will be with", patriName);
+}
+
+ghotok(patri, "hena");
+ghotok(patri, "rohima");
